fffbdebaefc99c0725b323b314710ae7c3a87248
onifade-joshua <onifadejoshua15@gmail.com>
2023-09-12 10:46:02 +0100

    Add project files.

---
+++ workingWithCasting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workingWithCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberVolume = 100;
+            Console.WriteLine("What's your random number");
+            var userResponse = Console.ReadLine();
+            if (int.TryParse(userResponse, out numberVolume)) //I'm trying to cast (the string value from the user input into an int by using TryParse()); 
+            {
+                Console.WriteLine("Hey you're successful!");
+            }
+            else
+            {
+                Console.WriteLine("Try a number next time!");
+            }
+            Console.WriteLine("Enter any key to continue....");
+            Console.ReadKey();
+        }
+    }
+}
